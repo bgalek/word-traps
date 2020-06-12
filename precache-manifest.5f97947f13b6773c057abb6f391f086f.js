5e5f07d804a5241b466b17325f6ccba482662288
Bartosz Gałek <bartosz@galek.com.pl>
2020-06-12 11:23:13 +0200

    Updates

---
+++ precache-manifest.5f97947f13b6773c057abb6f391f086f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba33ae7e6f85f73d4ae037edc7f1a776",
+    "url": "/word-traps/index.html"
+  },
+  {
+    "revision": "26e04a6078bbca15e419",
+    "url": "/word-traps/static/css/main.289402f2.chunk.css"
+  },
+  {
+    "revision": "cc5c0106933a72692353",
+    "url": "/word-traps/static/js/2.5d510529.chunk.js"
+  },
+  {
+    "revision": "dcd1d80362aef046a5450470cca1218a",
+    "url": "/word-traps/static/js/2.5d510529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e04a6078bbca15e419",
+    "url": "/word-traps/static/js/main.c3788d79.chunk.js"
+  },
+  {
+    "revision": "0601ef4e15490dc348a8",
+    "url": "/word-traps/static/js/runtime-main.e2cd6e7c.js"
+  },
+  {
+    "revision": "ff5abeb248b22a38c01f79e1bb8b417d",
+    "url": "/word-traps/static/media/logo.ff5abeb2.svg"
+  }
+]);
